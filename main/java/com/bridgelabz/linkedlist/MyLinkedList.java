eedddc6ea5b2263641248e0d5101e5ec08a05f06
sharale34 <sourabhharale8000@gmail.com>
2020-10-12 09:34:06 +0530

    Added: Ability to create Linked List by appending

--- main/java/com/bridgelabz/linkedlist/MyLinkedList.java
+++ main/java/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -20,4 +20,29 @@ public class MyLinkedList {
 			this.head.setNext(tempNode);
 		}
 	}
+
+	public void append(MyNode myNode) {
+		if (this.tail == null)
+			this.tail = myNode;
+		if (this.head == null)
+			this.head = myNode;
+		else {
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+
+	// Printing the available nodes
+	public void printNodes() {
+		StringBuffer myNodes = new StringBuffer("My Nodes : ");
+		INode tempNode = this.head;
+		while (tempNode.getNext() != null) {
+			myNodes.append(tempNode.getKey());
+			if (!tempNode.equals(tail))
+				myNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getKey());
+		System.out.println(myNodes);
+	}
 }
