079f782a0526b54a569b577527dcb1c36a4ddc62
sharale34 <sourabhharale8000@gmail.com>
2020-10-12 09:15:14 +0530

    Added: Created a simple linked list with three nodes

--- main/java/com/bridgelabz/linkedlist/MyNode.java
+++ main/java/com/bridgelabz/linkedlist/MyNode.java
@@ -1,7 +1,28 @@
 package com.bridgelabz.linkedlist;
 
-public class MyNode {
-public static void main(String[] args) {
-	System.out.println("Welcome to the Linked List Program");
-}
+public class MyNode<K> {
+	private K key;
+	private MyNode<K> next;
+
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public MyNode<K> getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode<K> next) {
+		this.next = next;
+	}
+
 }
---
+++ test/java/com/bridgelabz/linkedlist/MyNodeTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.linkedlist;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MyNodeTest {
+	@Test
+	public void given3NumbersWhenPassedShouldPassLinkedListTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+}
